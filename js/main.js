929049f888488a265890cedc0acbd70bde09e077
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-13 02:18:01 +0400

    restart

--- js/main.js
+++ js/main.js
@@ -8,18 +8,3 @@
 
 
 
-var ball = document.querySelector(".ball")
-
-var rightbtn =document.querySelector(".rightbtn")
-var leftbtn =document.querySelector(".leftbtn")
-
-
-rightbtn.addEventListener('click' , function(){ 
-    ball.style.left = '79%';
-
-
-});
-
-leftbtn.addEventListener('click', function () {
-    ball.style.left = '0%';
-});
